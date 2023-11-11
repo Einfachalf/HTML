fb3063ca0345fc4ef8eb79a5d31f96fb2e7d6316
einfachalf <einfachalf@gmail.com>
2023-11-11 17:51:36 +0100

    dvsv

---
+++ chat.js
@@ -0,0 +1,15 @@
+<script>
+                // Warten Sie, bis das DOM vollständig geladen ist
+                document.addEventListener('DOMContentLoaded', function() {
+                  // Erstellen Sie ein Skript-Element
+                  var script = document.createElement('script');
+                  script.src = "https://app.fastbots.ai/embed.js";
+                  script.setAttribute("data-bot-id", "clorqajvx005ju0b3u6qfbni9");
+                  script.setAttribute("data-fabicon", "http://d7keiwzj12p9.cloudfront.net/chatbubbles/photo_2022-04-27_08-51-18-1-1699606325916.webp");
+                  script.setAttribute("data-fab-position", "right");
+                  script.setAttribute("data-fabbg", "#730382");
+                  
+                  // Fügen Sie das Skript-Element zum body der Seite hinzu
+                  document.body.appendChild(script);
+                });
+              </script>
