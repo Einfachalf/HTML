187ad71a943895890123ca3abfbf0fb4cc56c9b3
Alex <148038194+Einfachalf@users.noreply.github.com>
2023-11-11 18:12:20 +0100

    Update chat.js

--- chat.js
+++ chat.js
@@ -1,4 +1,6 @@
 
 
 <script defer src="https://app.fastbots.ai/embed.js" 
-	data-bot-id="clorqajvx005ju0b3u6qfbni9"   ></script>
+	data-bot-id="clorqajvx005ju0b3u6qfbni9"  
+	data-fab-position="right" 
+	data-fabbg="#657be6"></script>
